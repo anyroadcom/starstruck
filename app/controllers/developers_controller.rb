f4a45fd4a27503efbeffa82f6125515f8fe54fd9
gkats <giannis.katsanos@gmail.com>
2015-07-24 16:27:06 +0300

    Added index and show actions

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,4 +1,16 @@
 class DevelopersController < ApiController::Base
+  before_filter :authorize!
+
+  def index
+    @developers = Developer.all
+    render json: @developers
+  end
+
+  def show
+    @developer = Developer.where(slug: params[:slug]).first!
+    render json: @developer
+  end
+
   def stars
     @developer = Developer.where(slug: params[:slug]).first!
     @developer.add_star!
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :developers, only: [:index]
   post '/:slug/stars' => 'developers#stars'
+  get '/:slug' => 'developers#show'
 
   match '*path', to: 'errors#not_found', via: :all
 end
